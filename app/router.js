50386f0df09eb0eccaa24665a2c8a5b35488106b
Chege <chegekformal@gmail.com>
2017-07-18 14:14:59 +0300

    First Upload. GET request is working.

--- app/router.js
+++ app/router.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+  location: config.locationType
 });
 
 Router.map(function() {
+  this.route('results', {path: '/results/:zip'});
 });
 
 export default Router;
---
+++ app/routes/results.js
@@ -0,0 +1,12 @@
+import config from '../config/environment';
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function(params) {
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +key+ '&zip=' +params.zip;
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.results;
+    });
+  }
+});
